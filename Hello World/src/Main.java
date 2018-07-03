b4d67ae2c0bbd2485e3b99e9e61d980fa287965f
CORPORATE\bab0914 <brian.bolhofner@daugherty.com>
2018-07-03 15:36:56 -0500

    initial push from work laptop

---
+++ Hello World/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+/*
+line one
+line two
+ */
+    public static void main(String[] args) {
+        //this is standalone
+        System.out.println("Hello World one!"); //this is a comment
+        //System.out.println("Hello World two!");
+        System.out.println(/*"Hello World three!"*/ "this text is changed");
+    }
+}
